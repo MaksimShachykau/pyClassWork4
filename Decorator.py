af6e3def4a01543484caace261b612a8a243d4cc
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-30 13:58:55 +0300

    count_decorator

--- Decorator.py
+++ Decorator.py
@@ -1,14 +1,11 @@
+import time
 
 
 # декоратор - это способ вызова функции внутри функции
 def my_decorator(my_function):
     def wrapper():
-        print("Start decoration - start function")
+        start_working = time.time()
         my_function()
-        print("End decoration - end function")
+        print("\nworking time = {}".format(str(start_working-time.time())))
     return wrapper()
 
-
-@my_decorator
-def bar():
-    print("Hello world")
