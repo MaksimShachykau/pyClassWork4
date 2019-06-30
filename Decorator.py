dd09bf83d610c020e6da7902a0df096e2500b681
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-30 14:15:12 +0300

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,17 +1,7 @@
-import time
+class Foo:
+    def __init__(self, bar):
+        self.bar = bar
 
-
-# декоратор - это способ вызова функции внутри функции
-def my_decorator(my_function):
-    def wrapper(*args):
-        start_working = time.time()
-        my_function(*args)
-        print("\nworking time = {}".format(str(-start_working+time.time())))
-    return wrapper()
-
-
-@my_decorator
-def foo(a=100):
-    for filename in range(a):
-        with open(str('file' + str(filename)), mode='a') as file:
-            file.write("String number {}".format(str(filename)))
+    @staticmethod
+    def method():
+        pass
