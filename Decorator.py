037ef04954c00e3da765774ce047b371340bebf7
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-30 15:53:16 +0300

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,7 +1,22 @@
-class Foo:
-    def __init__(self, bar):
-        self.bar = bar
+import time
+import os
+from threading import Thread
 
-    @staticmethod
-    def method():
-        pass
+
+def file_writer(filename, counter):
+    print("Srart t with PID {}".format(os.getpid()))
+    time.sleep(10)
+    with open(filename,'a') as f:
+        for line in range(counter):
+            f.write("String number {} \n".format(str(counter)))
+
+
+t = []
+for count in range(10):
+    t.append(Thread(target=file_writer, args=("File_number{}".format(1), count)))
+
+for thr in t:
+    thr.start()
+
+for thr in t:
+    thr.join()
