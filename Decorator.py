b4e04de477fb2ef7ed82eeb2a291b0668dbe97b7
IliaGodlevsky <godleevsky2015@gmail.com>
2019-06-30 14:07:14 +0300

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -6,10 +6,13 @@ def my_decorator(my_function):
     def wrapper(*args):
         start_working = time.time()
         my_function(*args)
-        print("\nworking time = {}".format(str(start_working-time.time())))
+        print("\nworking time = {}".format(str(-start_working+time.time())))
     return wrapper()
 
 
 @my_decorator
 def foo(a=10, b=100):
-    print(a**b)
+    item = 0
+    for item in range(5000000):
+        item += (item * a ** b)
+    print(item)
